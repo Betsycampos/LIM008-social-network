6a08ea2ac529796d5de941fde8c39ff89024f552
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-26 14:43:40 -0500

    Agregando el llamado mediante el DOM a los inputs y p warnings

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confPassword = document.getElementById('conf-password');
+
+const warningEmail = document.getElementById('warning-em');
+const warningPassword = document.getElementById('warning-pw');
+const warningConfirmP = document.getElementById('warning-cf');
+
+//Se debe importar la función de firebase
+//Antes de que se hagan las funciones debe haber la validación de
+
+
+if (email === '' && password === '' && confPassword === ''){
+    warningEmail.innerHTML = 'Completa el correo.';
+    warningPassword.innerHTML = 'Completa la contraseña.';
+    warningConfirmP.innerHTML = 'Confirma la contraseña ingresada.';
+}
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from './lib/index.js';
