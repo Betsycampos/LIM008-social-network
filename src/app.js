165d6d0b5cd206755d930152c2f7822e38e410ea
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-25 20:53:44 -0500

    Agregando firebase para fb - pruebas

--- src/app.js
+++ src/app.js
@@ -20,8 +20,33 @@ btnSubmit.addEventListener('click', () => {
 });
 const btnFb = document.getElementById('fb');
 btnFb.addEventListener('click', () => {
-    FB.getLoginStatus(function(response) {
-        console.log("esto es el obj resultado: " + statusChangeCallback(response));
+    // FB.getLoginStatus(function(response) {
+    //     console.log("esto es el obj resultado: " + statusChangeCallback(response));
          
-    });
+    // });
+    alert('hola');
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // console.log(token);
+        // The signed-in user info.
+        var user = result.user;
+        console.log('hola' + user);
+        
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+        // ...
+      });
 });
