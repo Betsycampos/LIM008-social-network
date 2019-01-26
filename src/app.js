08fa18b627abe0e8a11c3e4d3b018e24276cae99
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-25 21:00:42 -0500

    Agregando funciones para inicio de sesión con facebook

--- src/app.js
+++ src/app.js
@@ -49,4 +49,27 @@ btnFb.addEventListener('click', () => {
         console.log(credential);
         // ...
       });
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+        // ...
+      });
 });
