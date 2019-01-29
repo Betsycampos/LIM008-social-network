babe3cfcd199ac2ff8a2497f41c9737e731cac89
Heidy Gerbi <heidyjoanagr@gmail.com>
2019-01-28 23:08:21 -0500

    pruebas fb

--- src/main.js
+++ src/main.js
@@ -46,7 +46,61 @@ btnSubmit.addEventListener('click', () => {
   };
 });
 
-
+const btnFb = document.getElementById('fb');
+btnFb.addEventListener('click', () => {
+    // FB.getLoginStatus(function(response) {
+    //     console.log("esto es el obj resultado: " + statusChangeCallback(response));
+         
+    // });
+    alert('hola');
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // console.log(token);
+        // The signed-in user info.
+        var user = result.user;
+        console.log('hola' + user);
+        
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+        // ...
+      });
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+          // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+          var token = result.credential.accessToken;
+          // ...
+        }
+        // The signed-in user info.
+        var user = result.user;
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        console.log(errorCode);
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // The email of the user's account used.
+        var email = error.email;
+        console.log(email);
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(credential);
+        // ...
+      });
+});
 // Este es el punto de entrada de tu aplicacion
 
 // import { myFunction } from './lib/index.js';
