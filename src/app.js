dbb7234f492158f2c0bd4ff499c15a5def48ff8d
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-28 19:33:38 -0500

    Eliminando el evento del botón Registrar

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-const config = {
-  apiKey: "AIzaSyCrRlTuGI-KAzq1mzZ84cV68rReO1dp1e4",
-  authDomain: "redsocial-8c128.firebaseapp.com",
-  databaseURL: "https://redsocial-8c128.firebaseio.com",
-  projectId: "redsocial-8c128",
-  storageBucket: "redsocial-8c128.appspot.com",
-  messagingSenderId: "614216668686"
-};
-firebase.initializeApp(config);
+// const config = {
+//   apiKey: "AIzaSyCrRlTuGI-KAzq1mzZ84cV68rReO1dp1e4",
+//   authDomain: "redsocial-8c128.firebaseapp.com",
+//   databaseURL: "https://redsocial-8c128.firebaseio.com",
+//   projectId: "redsocial-8c128",
+//   storageBucket: "redsocial-8c128.appspot.com",
+//   messagingSenderId: "614216668686"
+// };
+// firebase.initializeApp(config);
 
 // export const createUser = (email, password) => {
 //   firebase.auth().createUserWithEmailAndPassword(email, password)
