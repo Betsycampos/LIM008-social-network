6955863006d1b6be2864951c91353f64c2447e3f
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-28 19:35:03 -0500

    Agregando el evento del botón y haciendo el llamado a las funciones

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const confPassword = document.getElementById('conf-password');
+const config = {
+  apiKey: "AIzaSyCrRlTuGI-KAzq1mzZ84cV68rReO1dp1e4",
+  authDomain: "redsocial-8c128.firebaseapp.com",
+  databaseURL: "https://redsocial-8c128.firebaseio.com",
+  projectId: "redsocial-8c128",
+  storageBucket: "redsocial-8c128.appspot.com",
+  messagingSenderId: "614216668686"
+};
+firebase.initializeApp(config);
+
+import { emailValidation, createUser } from './lib/index.js';
+// import { createUser } from 'app.js';
+
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
+const confPassword = document.getElementById('conf-password').value;
 
 const warningEmail = document.getElementById('warning-em');
 const warningPassword = document.getElementById('warning-pw');
@@ -9,14 +22,33 @@ const warningConfirmP = document.getElementById('warning-cf');
 //Se debe importar la función de firebase
 //Antes de que se hagan las funciones debe haber la validación de
 
+const btnSubmit = document.getElementById('btn-submit');
 
-if (email === '' && password === '' && confPassword === ''){
+btnSubmit.addEventListener('click', () => {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confPassword = document.getElementById('conf-password').value;
+  if (email === '' && password === '' && confPassword === ''){
     warningEmail.innerHTML = 'Completa el correo.';
     warningPassword.innerHTML = 'Completa la contraseña.';
     warningConfirmP.innerHTML = 'Confirma la contraseña ingresada.';
-}
+  } else if(password !== confPassword){
+    warningConfirmP.innerHTML = 'La contraseña ingresada debe coincidir con este campo.';
+  } else if (!emailValidation(email)){
+    warningEmail.innerHTML = 'El correo debe contener @ y .';
+  } else {
+    warningEmail.innerHTML = '';
+    warningPassword.innerHTML = '';
+    warningConfirmP.innerHTML = '';
+    createUser(email, password);
+    alert('Usuario registrado');
+    document.getElementById('frm-login').reset();
+  };
+});
+
+
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
