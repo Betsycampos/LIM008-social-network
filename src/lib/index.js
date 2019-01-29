2398c2a957540a11578e998cf4bba83fa3110c49
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-28 19:36:17 -0500

    Agregando la función createUser temporalmente

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,12 +1,18 @@
-
-
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-}
-  //Función para validar email correctos
-  export const emailValidation = (email) =>{
-    if (email.indexOf('@')===-1 || email.indexOf('.')===-1) return false;
+// Función para validar email correctos
+export const emailValidation = (email) => {
+  if (email.indexOf('@')===-1 || email.indexOf('.')===-1) return false;
     else if (email.indexOf('@') && email.indexOf('.')) return true;
-  };
+};
+
+export const createUser = (email, password) => {
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+};
