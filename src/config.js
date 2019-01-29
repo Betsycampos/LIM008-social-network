f94a063a9567004472bbca2e8baa08d6f958f4e1
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-29 18:08:03 -0500

    Agregando la linea que inicializa firebase y haciendola una función

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
-const config = {
+export const configFirebase = () => {
+  const config = {
     apiKey: "AIzaSyCrRlTuGI-KAzq1mzZ84cV68rReO1dp1e4",
     authDomain: "redsocial-8c128.firebaseapp.com",
     databaseURL: "https://redsocial-8c128.firebaseio.com",
     projectId: "redsocial-8c128",
     storageBucket: "redsocial-8c128.appspot.com",
     messagingSenderId: "614216668686"
-};
+  };
+   
+  firebase.initializeApp(config);
+}
