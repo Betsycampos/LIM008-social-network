8fb77220cbd96bb0dc291d2546d24bc01285a49f
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-02-01 16:46:19 -0500

    Agregamos rutas de wall y home

--- src/router.js
+++ src/router.js
@@ -1,27 +1,36 @@
 import register from './UI/register.js';
 import home from './UI/home.js';
 import login from './UI/login.js';
+import wall from './UI/wall.js';
 
-const viewTemplate = (routers) => {
+export const viewTemplate = (routers) => {
   const router = routers.substr(2, routers.length - 2);
   const container = document.getElementById('container');
   container.innerHTML = '';
-  switch (router){
-    case 'home':
+  switch (router) {
+    case 'home': {
       container.innerHTML = '';
       container.appendChild(home());
-    case 'register':
+      break;
+    }
+    case 'register': {
+      container.innerHTML = '';
       container.appendChild(register());
       break;
+    }
     case 'login': 
       container.appendChild(login());
+      break;
+    case 'wall':
+      container.appendChild(wall());
+      break;
   }
 };
 
 const changeTemplate = (hash) => {
-  if(hash === '#/' || hash === '' || hash === '#'){
+  if(hash === '#/' || hash === '' || hash === '#' || hash === '#/home'){
     return viewTemplate('#/home');
-  } else if(hash === '#/register' || hash === '#/login'){
+  } else if(hash === '#/register' || hash === '#/login' || hash === '#/wall'){
     return viewTemplate(hash);
   }
 };
