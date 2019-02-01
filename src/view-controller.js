fe7b6f5326d65f6d96da89197d07560db5357278
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-02-01 11:33:52 -0500

    Agregando las funciones que se usarán en la carpeta UI por los botones

---
+++ src/view-controller.js
@@ -0,0 +1,32 @@
+import { createUser } from './app.js';
+import { emailValidation } from './lib/index.js';
+
+export const registerWithEmailAndPassword = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confPassword = document.getElementById('conf-password').value;
+    const warningEmail = document.getElementById('warning-em');
+    const warningPassword = document.getElementById('warning-pw');
+    const warningConfirmP = document.getElementById('warning-cf');
+    if (email === '' && password === '' && confPassword === ''){
+      warningEmail.innerHTML = 'Completa el correo.';
+      warningPassword.innerHTML = 'Completa la contraseña.';
+      warningConfirmP.innerHTML = 'Confirma la contraseña ingresada.';
+    } else if(password !== confPassword){
+      warningConfirmP.innerHTML = 'La contraseña ingresada debe coincidir con este campo.';
+    } else if (!emailValidation(email)){
+      warningEmail.innerHTML = 'El correo debe contener @ y .';
+    } else {
+      warningEmail.innerHTML = '';
+      warningPassword.innerHTML = '';
+      warningConfirmP.innerHTML = '';
+      createUser(email, password);
+      alert('Usuario registrado');
+      document.getElementById('frm-register').reset();
+    };
+};
+
+// Esta es la función para que el usuario ingrese a la página con correo y contraseña
+export const loginUserWithEmailAndPassword = () => {
+
+}
