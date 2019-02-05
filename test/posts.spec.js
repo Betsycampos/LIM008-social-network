7cb9c24c6bc2beea311ba5929b21f7649d59681c
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-02-04 20:22:58 -0500

    Cambiando el nombre de las funciones deletePublish y getPublish

--- test/posts.spec.js
+++ test/posts.spec.js
@@ -2,22 +2,18 @@ import MockFirebase from 'mock-cloud-firestore';
 
 const data = {
     __collection__: {
-      onlyme: {
+      posts: {
         __doc__: {
           p001: {
             email: 'claudiamalasquez67@gmail.com',
             post: 'A veces parece que a nadie le importa el planeta.',
             countLikes: 0
           },
-        }
-      },
-      friends: {
-        __doc__: {
           p002: {
             email: 'alegna07@gmail.com',
             post: 'Amigos, haré una fiesta de disfraces hechos por materiales reciclables, todos pueden venir con algún disfraz o con algún material que podamos reciclar, el jueves a las 8pm, los espero!',
             countLikes: 0
-          },    
+          }
         }
       }
     }
@@ -25,17 +21,17 @@ const data = {
 
 global.firebase = new MockFirebase(data, { isNaiveSnapshotListenerEnabled: true });  
 
-import { deletePost, getPosts } from '../src/firebase-controller.js';
+import { deletePublish, getPublish } from '../src/firebase-controller.js';
 
 describe('Muro de posts', () => {
   it('Debería poder eliminar un post', (done) => {
-    return deletePost('onlyme', 'p001')
-      .then(() => getPosts(
+    return deletePublish('p001')
+      .then(() => getPublish(
         (data) => {
           const result = data.find((post) => post.id === 'p001');
           expect(result).toBe(undefined);
           done();
         }
-      , 'onlyme'))
+      ))
   });
 });
