7cf905bfe87c343fefbe3699b4b487c5121c38ce
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-02-08 21:10:48 -0500

    Agregando la función para ver los likes

--- src/firebase-controller.js
+++ src/firebase-controller.js
@@ -4,33 +4,40 @@ export const createUser = (email, password) =>
 export const signIn = (email, password) => 
   firebase.auth().signInWithEmailAndPassword(email, password)
 
+export const createProfile = (email, name) =>
+  firebase.firestore().collection('user').add({
+      email: email,
+      name: name
+  })
+
 export const authenticationGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider)
+  return firebase.auth().signInWithPopup(provider)
     .then((result) => {
-      const user = result.user.displayName;
       console.log(result);
+      const user = result.user.displayName;
+      console.log(user);
+      const email = result.user.email;
+      console.log(email);
+      createProfile(email, user);
+      console('Se registró con Google');
     })
-    .catch((error) => {
-      console.log(error);
-    })
+    .catch(() => { });
 };
 
 export const authenticationFacebook = () => {
   const provider = new firebase.auth.FacebookAuthProvider();
-  firebase.auth().signInWithPopup(provider)
+  return firebase.auth().signInWithPopup(provider)
     .then((result) => {
-      const token = result.credential.accessToken;
-      const user = result.user;
+      console.log(result);
+      const user = result.user.displayName;
       console.log(user);
+      const email = result.user.email;
+      console.log(email);
+      createProfile(email, user);
+      console('Se registró con Facebook');
     })
-    .catch((error) => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      const email = error.email;
-      const credential = error.credential;
-      console.log(error);
-    });
+    .catch(() => { });
 };
 
 export const signInWithFacebook = () => {
@@ -58,20 +65,12 @@ export const userData = () => {
 };
 
 export const addPublish = (email, textNewPublish, security) => 
-  // const email = userData();
   firebase.firestore().collection('posts').add({
     email: email,
     post: textNewPublish,
     countLikes: 0,
     security: security
   })
-  // .then((docRef) =>{
-  //   alert('Su post se agrego con éxito ', docRef.id);
-  // } )
-  // .catch((error) =>{
-  //   alert('Su post no puede ser publicado, Este es un gran error: ', error);
-  // })
-
 
 export const getPublish = (callback) =>
   firebase.firestore().collection('posts')
@@ -83,6 +82,12 @@ export const getPublish = (callback) =>
       callback(data);
     }); 
 
+
+export const editPublish = (idPost, textEditPost) => 
+  firebase.firestore().collection("posts").doc(idPost).update({
+    post:  textEditPost
+  });
+  
 export const deletePublish = (idPost) => 
   firebase.firestore().collection('posts').doc(idPost).delete()  
 
@@ -90,15 +95,22 @@ export const signOut = () => {
   firebase.auth().signOut().then(() => {
     alert('Se ha cerrado correctamente');
   })
-  .catch(err => console.log('Error logout', err))
-}
+    .catch(err => console.log('Error logout', err))
+};
 
-export const editPublish = (idPost, textEditPost) => 
-  firebase.firestore().collection("posts").doc(idPost).update({
-  post:  textEditPost
-});
+export const seeLikes = (idPost) => {
+  return firebase.firestore().collection("posts").doc(idPost).get()
+    .then((result) => {
+      if (result.exists) {
+        const likes = result.data().countLikes;
+        return likes;
+      } else {
+        console.log('No existe documento')
+      }
+    })
+};
 
+// Función para que los likes aumenten 
+export const increaseLikes = (idPost) => {
 
-
-
-
+};
