970a8709450014da027a1d80c75c5c62e2601e93
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-02-08 21:15:19 -0500

    Agregando una prueba de interfaz para mostrar likes

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,11 +1,21 @@
 // Función para validar email correctos
-  export const emailValidation = (email) =>{
-    if (email.indexOf('@') === -1 || email.indexOf('.') === -1) return false;
-    else return true;
-  };
+export const emailValidation = (email) =>{
+  if (email.indexOf('@') === -1 || email.indexOf('.') === -1) return false;
+  else return true;
+};
 
 // Función para validar la contraseña
-  export const passwordValidation = (password) =>{
-    if (password.length >= 6) return true;
-    else return false;
-  };
+export const passwordValidation = (password) => {
+  if (password.length >= 6) return true;
+  else return false;
+};
+
+
+// Prueba para mostrar like en interfaz
+
+// import { seeLikes } from '../firebase-controller.js';
+
+// document.getElementById('mostrarLikes').addEventListener('click', () => {
+//   const likes = seeLikes('PhLYPUbGROiK4g54yVtp')
+//     .then((result) => document.getElementById('likes').innerHTML = result)
+// });
