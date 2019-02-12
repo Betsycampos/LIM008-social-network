789db45ee59243089024699a6daa3a30e0d8af9d
Betsycampos <cbetsydalia@gmail.com>
2019-02-11 20:40:23 -0500

    Creando la función securityPost para filtrar seguridad

--- src/firebase-controller.js
+++ src/firebase-controller.js
@@ -4,12 +4,13 @@ export const createUser = (email, password) =>
 export const signIn = (email, password) => 
   firebase.auth().signInWithEmailAndPassword(email, password)
 
-export const createProfile = (email, name, photo = '') =>
+export const createProfile = (email, name, photo = '') => { 
 firebase.firestore().collection('user').add({
   email: email,
   name: name,
   photo: photo
 })
+};
 
 export const authenticationGoogle = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -104,3 +105,124 @@ export const editPublish = (idPost, textEditPost) =>
   firebase.firestore().collection("posts").doc(idPost).update({
   post:  textEditPost
 });
+
+// PRUEBA4
+export const securityPost = (type, callback) => 
+  firebase.firestore().collection('posts')
+  .where('security', '==', type)
+  .then((querySnapshot) => {
+    const data = [];
+    querySnapshot.forEach((doc) => {
+      data.push({ id: doc.id, ...doc.data()});
+      return data;
+    })
+    callback(data)
+  })
+
+
+
+  
+
+ 
+    // .orderBy('date', 'desc')
+//     .onSnapshot((querySnapshot) => {
+//       const data = [];
+//       querySnapshot.forEach((doc) => {
+//         data.push({ id: doc.id, ...doc.data()});
+//       });
+    
+//     });
+//     console.log(securityPost);
+// //PRUEBA3
+// export const securityPost = (type) => {
+//   firebase.firestore()
+//     .collection('posts')
+//     .where('security', '==', type)
+//     // .orderBy('', '')
+//     .onSnapshot(querySnapshot => {
+//       const ul = document.querySelector('#idPost');
+//       ul.innerHTML = '';
+//       const data = [];
+//       querySnapshot.forEach((doc) => {
+//         data.push({ id: doc.id, ...doc.data() });
+//       });
+//       data.forEach((security) => {
+//         ul.appendChild((security)); 
+//       });
+//     });
+// };
+// console.log(securityPost);
+//PRUEBA 2
+// export const securityPost = (type) => {
+//   firebase.firestore()
+//     .collection('posts')
+//     .where('security', '==', type)
+//     // .orderBy('', '')
+//     .onSnapshot(querySnapshot => {
+//       const data = [];
+//       querySnapshot.forEach((doc) => {
+//         data.push({ id: doc.id, ...doc.data() });
+//       });
+//       type(data);
+//     });
+// };
+// console.log(securityPost);
+//VISTA DE PUBLICACIONES AL PÚBLICO
+// export const securityPost = (type) => {
+//   firebase.firestore()
+//     .collection("posts")
+//     .where("all-public", "==", type)
+//     .get()
+//     .onSnapshot(querySnapshot => {
+//       const ul = document.querySelector()
+//     })
+//     // .then(function(querySnapshot) {
+//         const data = [];
+//         querySnapshot.forEach(doc) => {
+//             // doc.data() is never undefined for query doc snapshots
+//             data.push({ id: doc.id, ...doc.data() });
+//         });
+//     // })
+//     // .catch(function(error) {
+//     //     console.log("Error getting documents: ", error);
+//     // });
+// };
+// export const clasicationOfMe = service cloud.firestore() {
+//   match /databases/{database}/documents {
+//     match /{document=**} {
+//       allow read, write;
+//     }
+//   }
+// }
+// service cloud.firestore() {
+//   match /databases/{database}/documents {
+//     match /<some_path>/ {
+//       allow read, write: if <some_condition>;
+//     }
+//   }
+// }
+// export const postsOnlyMe = (email, textNewPublish, security) => 
+//   firebase.firestore().collection('posts').get({
+//     email: email,
+//     post: textNewPublish,  
+//     countLikes: 0,
+//     security: security
+//   })
+// console.log(postsOnlyMe); 
+
+// export const ostsOnlyMe = (data, filterBy, condition) => {
+//   let onlyMe = [];
+//   switch (filterBy) {
+//   case 'only-me':
+//   onlyMe = data.filter(type => type.security[0] === condition || type.tags[1] === condition);
+//     break;
+//   case 'friends':
+//   onlyMe = data.filter();
+//     break;
+//   }
+//   console.log(postsOnlyMe);
+//   return onlyMe;
+  
+// };
+
+ 
