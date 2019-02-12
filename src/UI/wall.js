869e5fdcee76d86dc052e2f5a693b861c54c80d4
Betsycampos <cbetsydalia@gmail.com>
2019-02-11 20:39:02 -0500

    Importando la funcion securityPost}

--- src/UI/wall.js
+++ src/UI/wall.js
@@ -1,4 +1,5 @@
 import { addPublishOnSubmit, deletePublishOnClick, editPublishOnClick } from "../view-controller.js";
+import {securityPost} from "../firebase-controller.js"
 
 const itemPublish = (objPublish) => {
   const divElement = document.createElement('div');
@@ -27,7 +28,9 @@ const itemPublish = (objPublish) => {
      
       const btnSave = document.getElementById(`btn-save-${objPublish.id}`);
        btnSave.addEventListener('click', () => editPublishOnClick(objPublish));  
-       
+      //  divElement.querySelector('#post-all')
+      // .addEventListener('click', securityPost);
+    
     });
     
     
@@ -49,9 +52,11 @@ export default (post) => {
     <textarea id="txt-post" cols="30" rows="10" placeholder="¿Qué quieres publicar?"></textarea>
     <div class="align-left">
       <select id="select-security" class="select-security">
-        <option value="only-me">Solo yo</option>
+        <option id="me"value="only-me">Solo yo</option>
         <option value="friends">Amigos</option>
       </select>
+      <button type= "button" id="post-me" class="btn-only">Solo yo</button>
+      <button type= "button" id="post-friends class="btn-friends">Amigos</button>     
       <button id="btn-publish" type="button" class="btn-wall">Publicar</button>
     </div>
   </div>
@@ -63,16 +68,38 @@ export default (post) => {
   <!-- snakbar -->
   <div id="demo-snackbar">
   </div>`;
+  
   formElem.setAttribute('id', 'frm-wall');
   formElem.innerHTML = formContent;
   const btnPublish = formElem.querySelector('#btn-publish');
   btnPublish.setAttribute('class', 'btn-wall');
-  const divContinerPost = formElem.querySelector('#publish-list');
-  post.forEach(data => {
-    divContinerPost.appendChild(itemPublish(data));
+  const divContainerPost = formElem.querySelector('#publish-list');
+  formElem.querySelector('#post-me').addEventListener('click',() => {
+ 
   });
+  
+
+  //return securityOnClick('friends')
+    //.then((result) => {
+    
+      //console.log(result)
+    //  result.forEach(data => {
+    //       divContainerPost.appendChild(itemPublish(data));
+    //    });
+    //})
+    // securityArray.forEach(data => {
+    //   divContainerPost.appendChild(itemPublish(data));
+    // });
+  
+  
+
+  post.forEach(data => {
+    divContainerPost.appendChild(itemPublish(data));
+  });
+  
   btnPublish.addEventListener('click', addPublishOnSubmit);
   return formElem;
+  
 }
 
 
